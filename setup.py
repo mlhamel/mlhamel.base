0a55ff1394190fcb7f8a880e132ee4dd95e09a78
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2015-07-15 11:52:04 -0400

    Bumped version: 0.1.0 -> 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
 
 setup(
     name="mlhamel.base",
-    version="0.1.0",
+    version="0.2.0",
     description="mlhamel.base image for docker",
     long_description=README + "\n\n",
     author="Mathieu Leduc-Hamel",
